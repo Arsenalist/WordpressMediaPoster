f852efd2f0e82c3c44d50757e0865a9704c6170e
Arsenalist <root@raptorsrepublic.com>
2017-09-30 23:23:31 -0400

    Latest changes

--- media_sources.py
+++ media_sources.py
@@ -2,161 +2,405 @@ import subprocess
 import re
 import requests
 from bs4 import BeautifulSoup
+import HTMLParser
+from urlparse import urlparse
 
-
+from urlparse import urlparse, parse_qs
 class MediaPost:
-	def get_meta_value(self, prop):
-		meta = self.soup.find('meta', property=prop)
-		return meta['content']
+  def get_meta_value(self, prop):
+    meta = self.soup.find('meta', property=prop)
+    if meta:
+        return meta['content']
+    else:
+        return ''
 
-	def get_thumb(self):
-		return self.get_meta_value('og:image')
+  def get_thumb(self):
+    return self.get_meta_value('og:image')
 
-	def get_embed(self):
-		raise NotImplementedError( "Must implement get_embed" )
+  def get_embed(self):
+    raise NotImplementedError( "Must implement get_embed" )
 
-	def get_soup(self):
-		r = requests.get(self.url)
-		return BeautifulSoup(r.text)
+  def get_soup(self):
+    r = requests.get(self.url, verify=False)
+    self.text = r.text
+    return BeautifulSoup(r.text)
 
-	def get_json(self):
-		r = requests.get(self.url)
-		return r.json()
+  def get_json(self):
+    r = requests.get(self.url, verify=False)
+    return r.json()
+
+  def get_type(self):
+    return "Video"
+
+class SimpleMediaPost(MediaPost):
+    def __init__(self, url, embed):
+        self.url = url
+        self.embed = embed
+
+    def get_thumb(self):
+        return self.url
+
+    def get_embed(self):
+        return self.embed
+
+class LiveTV(MediaPost):
+
+  def __init__(self, url):
+    self.url = url
+    self.soup = self.get_soup()
+    self.container_soup = self.find_video_container_soup()
+    self.init_attrs()
+
+  def find_video_list_container(self, tag):
+    return tag.has_attr("bgcolor") and tag["bgcolor"] == "#aaaaaa"
+
+  def find_video_container_soup(self):
+    return self.soup.find(self.find_video_list_container).find_all("tr")[0].find("td")
+
+  def init_attrs(self):
+     if "playwire" in str(self.container_soup):
+        script = self.container_soup.find("script")
+        config_url = script["data-config"]
+        json = requests.get(config_url, verify=False).json()
+        self.thumb = json["poster"]
+        self.embed = str(script)
+
+     elif "vk.com" in str(self.container_soup):
+        iframe = self.container_soup.find("iframe")
+        url = iframe["src"]
+        vk = Vk(url)
+        self.thumb = vk.get_thumb()
+        self.embed = vk.get_embed()
+
+     elif "youtube.com" in str(self.container_soup):
+        iframe = self.container_soup.find("iframe")
+        url = iframe["src"]
+        query = urlparse(url)
+        video_id = query.path.split('/')[2]
+        youtube = YouTube(video_id)
+        self.thumb = youtube.get_thumb()
+        self.embed = youtube.get_embed()
+
+     elif "dailymotion.com" in str(self.container_soup):
+        iframe = self.container_soup.find("iframe")
+        url = iframe["src"]
+        query = urlparse(url)
+        video_id = query.path.split('/')[3]
+        dm = DailyMotion(video_id)
+        self.thumb = dm.get_thumb()
+        self.embed = dm.get_embed()
+
+     elif "rutube.ru" in str(self.container_soup):
+        iframe = self.container_soup.find("iframe")
+        url = iframe["src"]
+        query = urlparse(url)
+        video_id = query.path.split('/')[3]
+        ru = Rutube(video_id)
+        self.thumb = 'http://i.imgur.com/UsIqkhm.jpg' #ru.get_thumb()
+        self.embed = str(iframe)
+
+
+
+  def get_thumb(self):
+    return self.thumb
+  
+  def get_embed(self):
+    return self.embed
+
+class Vk(MediaPost):
+
+  def __init__(self, embed_url):
+    self.embed_url = embed_url
+    self.urlparse = urlparse(self.embed_url)
+    params = parse_qs(self.urlparse.query)
+    oid = params['oid'][0]
+    id = params['id'][0]
+    self.url = 'http://vk.com/video' + oid + '_' + id
+    self.soup = self.get_soup()
+
+  def get_embed(self):
+    width = self.get_meta_value("og:video:width")
+    height = self.get_meta_value("og:video:height")
+    return '<iframe src="' + self.embed_url + '" width="' + width + '" height="' + height + ' " frameborder="0"></iframe>'
+
+
+
+class Videa(MediaPost):
+
+  def __init__(self, url):
+    self.url = url
+    self.soup = self.get_soup()
+
+
+  def get_embed(self):
+    result = self.soup.find(id="player_embed_input")
+    if result and result.string:
+       return HTMLParser.HTMLParser().unescape(result.string)
+    else:
+      return None
+
 
 class LastMinuteGoals(MediaPost):
 
-    def __init__(self, video_id):
-        self.video_id = video_id
-        self.url = self.video_id
-        self.soup = self.get_soup()
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = self.video_id
+    self.soup = self.get_soup()
+    self.urlparse = urlparse(self.url)
 
 
-    def get_embed(self):
-        result = self.soup.find_all('script', src=re.compile("playwire"))
-        if len(result) != 0:
-            return unicode(result[0])
-        else:
-            return None
+  # Nasty piece of work which gets the HTML that BeautifulSoup can actually parse
+  def get_specific_html(self):
+    lines = self.text.split("\n")
+    start_line = 0;
+    end_line = 0
+    for idx, l in enumerate(lines):
+      if "tabs_" in l:
+        start_line = idx
+        break
+    for idx, l in enumerate(lines):
+      if "</div><script" in l and idx > start_line:
+        end_line = idx
+        break
+    segment = lines[start_line:end_line+1]
+    html = ""
+    for l in segment:
+      html = html + l
+    return html
 
+  def get_embed(self):
+    soup = BeautifulSoup(self.get_specific_html())
+    # No fragment means get the second one (first is something weird)
+    if self.urlparse.fragment == '':
+      result = soup.find_all('div', id=re.compile("tabs"))
+      div = result[1]
+    else:
+      div = soup.find(id=self.urlparse.fragment)
+
+    if div:
+      script = div.find_all(["script", "iframe"])
+      if script and len(script) != 0:
+        html = ""
+        for s in script:
+            html = html + unicode(s) + "<br/>"
+        return html
+
+    return None
+
+
+    
 class Webm(MediaPost):
-	def __init__(self, url):
-		self.url = url
+  def __init__(self, url):
+    self.url = url
 
-	def get_thumb(self):
-		return "/assets/nothumb.jpg"
-		
-	def get_embed(self):
-		return '<video width="auto" height="auto" controls loop autoplay><source src="' + self.url + '" type="video/webm" /></video>'
+  def get_thumb(self):
+    return "/assets/nothumb.jpg"
+    
+  def get_embed(self):
+    return '<video width="auto" height="auto" controls loop autoplay><source src="' + self.url + '" type="video/webm" /></video>'
+
+class Mp4(MediaPost):
+  def __init__(self, url):
+    self.url = url
+
+  def get_thumb(self):
+    return "http://i.imgur.com/Ej1oWfr.jpg"
+    
+  def get_embed(self):
+    return '<link href="http://vjs.zencdn.net/5.11.6/video-js.css" rel="stylesheet"><script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script><video id="my-video" class="video-js" controls preload="auto" width="640" height="360" data-setup="{}"><source src="' + self.url + '"><p class="vjs-no-js">Please enable JavaScript</p></video><script src="http://vjs.zencdn.net/5.11.6/video.js"></script>'
+
+
+
 
 
 class Gif(MediaPost):
-	def __init__(self, url):
-		self.url = url
+  def __init__(self, url):
+    self.url = url
 
-	def get_thumb(self):
-		output = subprocess.Popen(["./gif.sh", self.url], stdout=subprocess.PIPE).communicate()[0]
-		return output.split("\n")[0]
-		
-	def get_embed(self):
-		return '<img src="' + self.url +'"/>'
-		
+  def get_thumb(self):
+    output = subprocess.Popen(["/root/allthegoalspostergit/gif.sh", self.url], stdout=subprocess.PIPE).communicate()[0]
+    return output.split("\n")[0]
+    
+  def get_embed(self):
+    return '<img src="' + self.url +'"/>'
+
+  def get_type(self):
+    return "GIF"
+
+    
+class Image(MediaPost):
+  def __init__(self, url):
+    self.url = url
+
+  def get_thumb(self):
+    output = subprocess.Popen(["/root/allthegoalspostergit/gif.sh", self.url], stdout=subprocess.PIPE).communicate()[0]
+    return output.split("\n")[0]
+    
+  def get_embed(self):
+    return '<img src="' + self.url +'"/>'
+
+  def get_type(self):
+    return "Image"
+
+
+class MoeVideo(MediaPost):
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = 'http://moevideo.net/video/' + self.video_id
+    self.soup = self.get_soup()
+
+  def get_embed(self):
+    return '<iframe width="640" height="360" src="http://moevideo.net/framevideo/' + self.video_id + '?width=640&#038;height=360"  frameborder="0" allowfullscreen ></iframe>'
+
+
+
+class VidMe(MediaPost):
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = 'http://vid.me/' + self.video_id
+    self.soup = self.get_soup()
+
+  def get_embed(self):
+    result = self.soup.select('.video_actions_embed a')
+    if len(result) != 0:
+        width = result[0]['data-width']
+        height = result[0]['data-height']
+        return '<iframe src="https://vid.me/e/' + self.video_id + '" width="' + width + '" height="' + height + '" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen scrolling="no"></iframe>'
+
+
+
+
 class Vimeo(MediaPost):
-	def __init__(self, video_id):
-		self.video_id = video_id
-		self.url = 'http://vimeo.com/' + self.video_id
-		self.soup = self.get_soup()
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = 'http://vimeo.com/' + self.video_id
+    self.soup = self.get_soup()
 
-	def get_embed(self):
-		return '<iframe src="//player.vimeo.com/video/' + self.video_id + '" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+  def get_embed(self):
+    return '<iframe src="//player.vimeo.com/video/' + self.video_id + '" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+
 
 
 class MediaCrush(MediaPost):
-	def __init__(self, video_id):
-		self.video_id = video_id
-		self.url = 'https://mediacru.sh/' + self.video_id
-		self.soup = self.get_soup()
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = 'https://mediacru.sh/' + self.video_id
+    self.soup = self.get_soup()
 
-	def get_embed(self):
-		width = self.get_meta_value('twitter:player:width')
-		height = self.get_meta_value('twitter:player:height')
-		return '<iframe width="' + width + '" height="' + height + '" src="https://mediacru.sh/' + self.video_id + '/frame frameborder="0" allowFullscreen></iframe>'
+  def get_embed(self):
+    width = self.get_meta_value('twitter:player:width')
+    height = self.get_meta_value('twitter:player:height')
+    return '<iframe width="' + width + '" height="' + height + '" src="https://mediacru.sh/' + self.video_id + '/frame frameborder="0" allowFullscreen></iframe>'
 
 class Rutube(MediaPost):
-	def __init__(self, video_id):
-		self.video_id = video_id
-		self.url = 'http://rutube.ru/video/' + self.video_id + '/'
-		self.soup = self.get_soup()
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = 'http://rutube.ru/video/' + self.video_id + '/'
+    self.soup = self.get_soup()
 
-	def get_embed(self):
-		result = self.soup.select('#embed-field iframe')
-		if len(result) != 0:
-			return unicode(result[0])
-		else:
-			return None
+  def get_embed(self):
+    result = self.soup.select('#embed-field iframe')
+    if len(result) != 0:
+      return unicode(result[0])
+    else:
+      return None
 
 
 class Facebook(MediaPost):
-	def __init__(self, video_id):
-		self.video_id = video_id
-		self.url =  'https://graph.facebook.com/' + self.video_id + '/?access_token=CAACEdEose0cBAECdQm4uWC7dZCPBFzQwZAgBXvRO6UBT98hUPSBZAnNGrHSPYDTbll6SqSnzS0uR5JJcftYEByUh9eJuvbDGJr6gkgz0FXBZB7hFjlEXbKN0BekPX3tZCF4XJ9Fz2FJcCBTuJ6z4X3y7nv7zvu6dIsMxtpT1ZADq5pQH8sguKbhGmqmo4wwasZD'
-		self.json = self.get_json()
+  def __init__(self, video_id):
+    self.video_id = video_id
+    
+    #graph = facebook.GraphAPI('CAADX6q2xheEBACjERsor8CZBsxzoZC3Tw7JSsDY4q1Scu3LlVQS34XYjwxyQMeqvoi0odIkWXOZApMWdrdixvS5HuN7d7yKuo4qbtGAYpKAk7sVZBikoNgk1GWXy9xxdgr8S0w2PmyPxBaHCUF5x0Laszw0gIyDrmXFdsKPjGcTnJpIumSvVZByxKJxuPhikZD')
+    self.url =  'https://graph.facebook.com/' + self.video_id + '/?access_token=CAADX6q2xheEBACjERsor8CZBsxzoZC3Tw7JSsDY4q1Scu3LlVQS34XYjwxyQMeqvoi0odIkWXOZApMWdrdixvS5HuN7d7yKuo4qbtGAYpKAk7sVZBikoNgk1GWXy9xxdgr8S0w2PmyPxBaHCUF5x0Laszw0gIyDrmXFdsKPjGcTnJpIumSvVZByxKJxuPhikZD'
+    self.json = self.get_json()
 
-	def get_thumb(self):
-		return self.json["picture"]
+  def get_thumb(self):
+    return self.json["picture"]
 
 
-	def get_embed(self):
-		return self.json['embed_html']
+  def get_embed(self):
+    return self.json['embed_html']
 
 
 class Vine(MediaPost):
-	def __init__(self, video_id):
-		self.video_id = video_id
-		self.url = 'https://vine.co/v/' + self.video_id
-		self.soup = self.get_soup()
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = 'https://vine.co/v/' + self.video_id
+    self.soup = self.get_soup()
 
-	def get_embed(self):
-		return '<iframe class="vine-embed" src="https://vine.co/v/' + self.video_id +'/embed/simple" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>'
+  def get_embed(self):
+    return '<iframe class="vine-embed" src="https://vine.co/v/' + self.video_id +'/embed/simple" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>'
+
+  def get_type(self):
+    return "Vine"
+
+
+class Streamable(MediaPost):
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = 'http://streamable.com/' + self.video_id
+    self.soup = self.get_soup()
+
+  def get_embed(self):
+    result = self.soup.find(id="embed")
+    #return result['value']  + "<center><a href='" + self.url + "'>Direct Link</a></center>"
+    return '<iframe src="https://streamable.com/e/' + self.video_id + '" width="640" height="360" frameborder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen="" scrolling="no" style=""></iframe>'
+
+  def get_thumb(self):
+
+    result = MediaPost.get_thumb(self)
+    return result
+
+
+
 
 
 class YouTube(MediaPost):
-	def __init__(self, video_id):
-		self.video_id = video_id
-		self.url = 'http://youtube.com/watch?v=' + self.video_id
-		self.soup = self.get_soup()
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = 'http://youtube.com/watch?v=' + self.video_id
+    self.soup = self.get_soup()
 
-	def get_embed(self):
-		return '<iframe width="560" height="315" src="//www.youtube.com/embed/' +  self.video_id + '" frameborder="0" allowfullscreen></iframe>'
+  def get_thumb(self):
+    return "http://img.youtube.com/vi/" + self.video_id + "/mqdefault.jpg"
+
+
+
+  def get_embed(self):
+    return '<iframe width="560" height="315" src="//www.youtube.com/embed/' +  self.video_id + '" frameborder="0" allowfullscreen></iframe>'
 
 
 class DailyMotion(MediaPost):
-	def __init__(self, video_id):
-		self.video_id = video_id
-		self.url = 'http://dailymotion.com/video/' + self.video_id
-		self.soup = self.get_soup()
+  def __init__(self, video_id):
+    self.video_id = video_id
+    self.url = 'http://dailymotion.com/video/' + self.video_id
+    self.soup = self.get_soup()
 
 
-	def get_embed(self):
-		width = self.soup.find('meta', property='og:video:width')
-		height = self.soup.find('meta', property='og:video:height')
-		return '<iframe frameborder="0" width="' + width['content'] + '" height="' + height['content'] + '" src="//www.dailymotion.com/embed/video/' + self.video_id + '" allowfullscreen></iframe>'
+  def get_embed(self):
+    width = self.soup.find('meta', property='og:video:width')
+    height = self.soup.find('meta', property='og:video:height')
+    return '<iframe frameborder="0" width="' + width['content'] + '" height="' + height['content'] + '" src="//www.dailymotion.com/embed/video/' + self.video_id + '" allowfullscreen></iframe>'
 
 
 class Gfy(MediaPost):
-	def __init__(self, gfy):
-		self.gfy = gfy
-		self.url = 'http://gfycat.com/' + self.gfy
-		self.soup = self.get_soup()
+  def __init__(self, gfy):
+    self.gfy = gfy
+    self.url = 'http://gfycat.com/' + self.gfy
+    self.soup = self.get_soup()
 
-	def get_thumb(self):
-		return 'http://thumbs.gfycat.com/' + self.gfy + '-thumb100.jpg'
+  def get_thumb(self):
+    return 'http://thumbs.gfycat.com/' + self.gfy + '-thumb100.jpg'
 
-	def get_embed(self):
-		return '<img class="gfyitem" data-id="' + self.gfy + '" />'
-		"""
-		result = self.soup.find_all('span', text=re.compile("iframe"), class_="gfyShareLinkText")
-		if len(result) != 0:
-			return unicode(result[0].string)
-		else:
-			return None
-		"""
+  def get_embed(self):
+    return '<img class="gfyitem" data-id="' + self.gfy + '" /><br/><center><a href="'+ self.url + '">Direct Link</a></center>'
+
+  def get_type(self):
+    return "GFY"
+
+class GfyWordpress(Gfy):
+  def get_embed(self):
+    return '[gfy]' + self.gfy + '[/gfy]'
+
+
+
--- poster.py
+++ poster.py
@@ -9,128 +9,148 @@ from urlparse import urlparse, parse_qs
 import media_sources
 
 
-class WordPressPoster:	
-	def __init__(self, date, primary_category_name, secondary_category_name=None):		
-		self.client = Client('http://www.instagoals.com/xmlrpc.php', 'admin', 'somepassword')
-		self.date = date
-		self.primary_category_name = primary_category_name
-		if secondary_category_name != None:
-			self.secondary_category_name = secondary_category_name + " - " + self.format_date(date)
-		else:
-			self.secondary_category_name = None
+class WordPressPoster:  
+    def __init__(self, date, primary_category_name, secondary_category_name=None):      
+        self.client = Client('http://www.instagoals.com/xmlrpc.php', 'admin', 'Arsenal14!$')
+        self.date = date
+        self.primary_category_name = primary_category_name
+        self.secondary_category_name = secondary_category_name
 
-	def format_date(self, date):
-		return datetime.datetime.strptime(date, '%Y-%m-%d').date().strftime("%B %d %Y")
+    def format_date(self, date):
+        return datetime.datetime.strptime(date, '%Y-%m-%d').date().strftime("%B %d %Y")
 
 
-	def create_category(self, category_name, parent_category_id=None):
-		category = WordPressTerm()
-		category.taxonomy = 'category'
-		if parent_category_id != None:
-			category.parent = parent_category_id
-		category.name = category_name
-		category.id = self.client.call(taxonomies.NewTerm(category))
-		return category
+    def create_category(self, category_name, parent_category_id=None):
+        category = WordPressTerm()
+        category.taxonomy = 'category'
+        if parent_category_id != None:
+            category.parent = parent_category_id
+        category.name = category_name
+        category.id = self.client.call(taxonomies.NewTerm(category))
+        return category
 
-	def get_category(self, category_name):
-		results = self.client.call(taxonomies.GetTerms('category', {'search': category_name}))
-		if len(results) == 0:
-			return None
-		else:
-			return results[0]
+    def get_category(self, category_name):
+        results = self.client.call(taxonomies.GetTerms('category', {'search': category_name}))
+        if len(results) == 0:
+            return None
+        else:
+            return results[0]
 
-	def find_terms(self):
-		cat_list = []
-		cat = self.get_category(self.primary_category_name)
-		if cat == None:
-			cat = self.create_category(self.primary_category_name, 25) # Competition
-		cat_list.append(cat)
+    def find_terms(self):
+        cat_list = []
+        cat = self.get_category(self.primary_category_name)
+        if cat == None:
+            cat = self.create_category(self.primary_category_name, 25) # Competition
+        cat_list.append(cat)
 
-		if self.secondary_category_name != None:
-			cat = self.get_category(self.secondary_category_name)
-			if cat == None:
-				cat = self.create_category(self.secondary_category_name, 7) # Matches
-			cat_list.append(cat)
-		return cat_list
+        if self.secondary_category_name != None:
+            cat = self.get_category(self.secondary_category_name)
+            if cat == None:
+                cat = self.create_category(self.secondary_category_name, 7) # Matches
+            cat_list.append(cat)
+        return cat_list
 
 
-	def create_match_post(self, media_post, title, tags, extra):
-		post = WordPressPost()
-		post.title = title
-		if extra == None:
-			extra = ""
-		post.content = media_post.get_embed() + extra
-		post.terms_names = {
-		        'post_tag': tags
-		}
 
-		post.terms = self.find_terms()
-		post.post_status = 'publish'
-		post.custom_fields = [
-			{
-				'key': 'thumbnail',
-				'value': media_post.get_thumb()
-			},
-			{
-				'key': 'event_date',
-				'value': self.date
-			}			
-		]
-		post.id = self.client.call(posts.NewPost(post))
-		return post
+    def create_match_post(self, media_post, title, tags, extra):
+        post = WordPressPost()
+        post.title = title
+        if extra == None:
+            extra = ""
+        post.content = media_post.get_embed() + extra
+        post.terms_names = {
+                'post_tag': tags
+        }
+
+        post.terms = self.find_terms()
+        post.post_status = 'publish'
+        post.custom_fields = [
+            {
+                'key': 'thumbnail',
+                'value': media_post.get_thumb()
+            },
+            {
+                'key': 'event_date',
+                'value': self.date
+            }           
+        ]
+        post.id = self.client.call(posts.NewPost(post))
+        return post
 
 
 class MediaPostFactory:
-	@staticmethod
-	def create(url):
-		if url.endswith('.webm'):
-			return media_sources.Webm(url)
-		u = urlparse(url)
-		params = parse_qs(u.query)
-		if u.hostname == 'www.youtube.com':
-			return media_sources.YouTube(params['v'][0])
-		elif u.hostname == 'www.lastminutegoals.org':
-			return media_sources.LastMinuteGoals(url)
-		elif u.hostname == 'www.facebook.com':
-			return media_sources.Facebook(params['video_id'][0])
-		elif u.hostname == 'www.dailymotion.com':
-			return media_sources.DailyMotion(u.path.split("/")[2])
-		elif u.hostname == 'gfycat.com' or u.hostname == 'www.gfycat.com':
-			return media_sources.Gfy(u.path.split("/")[1])
-		elif u.hostname == 'vine.co':
-			return media_sources.Vine(u.path.split("/")[2])
-		elif u.hostname == 'vimeo.com':
-			return media_sources.Vimeo(u.path.split("/")[1])
-		elif u.hostname == 'rutube.ru':
-			return media_sources.Rutube(u.path.split("/")[2])
-		elif u.hostname == 'mediacru.sh':
-			return media_sources.MediaCrush(u.path.split("/")[1])
-		else:
-			raise NotImplementedError("No MediaPost implemented for " + url) 
+    @staticmethod
+    def create(url):
+        if url.endswith('.webm'):
+            return media_sources.Webm(url)
+        if url.endswith('.mp4'):
+            return media_sources.Mp4(url)
+        if url.endswith('.gif'):
+            return media_sources.Gif(url)
+        if url.endswith('.jpg') or url.endswith('.png'):
+            return media_sources.Image(url)
+           
+        u = urlparse(url)
+        params = parse_qs(u.query)
+        print u.hostname
+        if u.hostname == 'www.youtube.com' or u.hostname == 'm.youtube.com':
+            return media_sources.YouTube(params['v'][0])
+        elif u.hostname == 'youtu.be':
+            return media_sources.YouTube(u.path.split('/')[1])
+        elif u.hostname == 'vid.me':
+            return media_sources.VidMe(u.path.split('/')[1])
+        elif u.hostname == 'moevideo.net':
+            return media_sources.MoeVideo(u.path.split('/')[2])
+        elif u.hostname == 'videa.hu':
+            return media_sources.Videa(url)
+        elif u.hostname == 'www.lastminutegoals.org':
+            return media_sources.LastMinuteGoals(url)
+        elif u.hostname == 'www.facebook.com':
+            if 'video_id' in params:
+                return media_sources.Facebook(params['video_id'][0])
+            elif 'v' in params:
+                return media_sources.Facebook(params['v'][0])
+        elif u.hostname == 'www.dailymotion.com':
+            return media_sources.DailyMotion(u.path.split("/")[2])
+        elif u.hostname == 'gfycat.com' or u.hostname == 'www.gfycat.com':
+            return media_sources.Gfy(u.path.split("/")[1])
+        elif u.hostname == 'streamable.com' or u.hostname == 'www.streamable.com':
+            return media_sources.Streamable(u.path.split("/")[1])
+        elif u.hostname == 'vine.co':
+            return media_sources.Vine(u.path.split("/")[2])
+        elif u.hostname == 'vimeo.com':
+            return media_sources.Vimeo(u.path.split("/")[1])
+        elif u.hostname == 'rutube.ru':
+            return media_sources.Rutube(u.path.split("/")[2])
+        elif u.hostname == 'mediacru.sh':
+            return media_sources.MediaCrush(u.path.split("/")[1])
+        elif u.hostname == 'livetv.sx':
+            return media_sources.LiveTV(url)
+        else:
+            raise NotImplementedError("No MediaPost implemented for " + url) 
 
 
 def main():
-	p = argparse.ArgumentParser(description='Create a HTML page containing a GIF.')
-	p.add_argument('-a', '--date', default=datetime.date.today().strftime('%Y-%m-%d'))
-	p.add_argument('-g', '--tags', nargs="*", default=[])
-	p.add_argument("-m", '--matchcategory', help="Match category", required=False)
-	p.add_argument("-u", '--source', help="Source", required=False)
+    p = argparse.ArgumentParser(description='Command')
+    p.add_argument('-a', '--date', default=datetime.date.today().strftime('%Y-%m-%d'))
+    p.add_argument('-g', '--tags', nargs="*", default=[])
+    p.add_argument("-m", '--matchcategory', help="Match category", required=False)
+    p.add_argument("-u", '--source', help="Source", required=False)
 
-	p.add_argument('-d', '--description', required=True)
-	p.add_argument("-s", '--sitecategory', help="Site category", required=True)
-	p.add_argument('url')
-	
-	
-	args = vars(p.parse_args())
-	source = None
-	if args['source']:
-		source = '<p><a href="' + args['source'] + '">Source</a></p>'
-	wpp = WordPressPoster(args['date'], args['sitecategory'], args['matchcategory'])
-	#print MediaPostFactory.create(args['url']).get_embed()
-	wpp.create_match_post(MediaPostFactory.create(args['url']), args['description'], args['tags'], source)
+    p.add_argument('-d', '--description', required=True)
+    p.add_argument("-s", '--sitecategory', help="Site category", required=True)
+    p.add_argument('url')
+    
+    
+    args = vars(p.parse_args())
+    source = None
+    if args['source']:
+        source = '<p><a href="' + args['source'] + '">Source</a></p>'
+    wpp = WordPressPoster(args['date'], args['sitecategory'], args['matchcategory'])
+    wpp.create_match_post(MediaPostFactory.create(args['url']), args['description'], args['tags'], source)
 
 if __name__ == '__main__':
-	main()
+    main()
 
 
 """h
